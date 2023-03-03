9fa895aa6d17f6d83d053eac98d62537bb07b573
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-03-02 20:34:40 -0800

    fix username

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ class CreatePayLinkData(BaseModel):
     success_text: str = Query(None)
     success_url: str = Query(None)
     fiat_base_multiplier: int = Query(100, ge=1)
-    username: str = Query(None)
+    username: str
 
 
 class PayLink(BaseModel):
