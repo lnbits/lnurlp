dcb79bcb32c3d6f166194f7e85317239274ffee0
Tiago Vasconcelos <talvasconcelos@gmail.com>
2024-10-04 12:51:48 +0100

    fix passing user obj

--- views.py
+++ views.py
@@ -1,11 +1,12 @@
 from http import HTTPStatus
 
 from fastapi import APIRouter, Depends, Request
+from starlette.exceptions import HTTPException
+from starlette.responses import HTMLResponse
+
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 from lnbits.helpers import template_renderer
-from starlette.exceptions import HTTPException
-from starlette.responses import HTMLResponse
 
 from .crud import get_pay_link
 
@@ -19,7 +20,7 @@ def lnurlp_renderer():
 @lnurlp_generic_router.get("/", response_class=HTMLResponse)
 async def index(request: Request, user: User = Depends(check_user_exists)):
     return lnurlp_renderer().TemplateResponse(
-        "lnurlp/index.html", {"request": request, "user": user.json()}
+        "lnurlp/index.html", {"request": request, "user": user.dict()}
     )
 
 
